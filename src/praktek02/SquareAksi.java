ea81b03c868502d609b39e6e3ce3d72d2498346e
Dita Annisaftia Rizky <32301025+Ditaannisaftiarizky@users.noreply.github.com>
2018-04-03 09:37:28 +0800

    Mengisi Nilai Atribut

--- src/praktek02/SquareAksi.java
+++ src/praktek02/SquareAksi.java
@@ -2,5 +2,8 @@ package praktek02;
 public class SquareAksi {
     public static void main(String[] args) {
         Square r1 = new Square();
+        r1.sisi1 = 5;
+        r1.sisi2 = 5;
+        
     }
 }

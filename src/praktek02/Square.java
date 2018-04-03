7b60c90ef1bfdbc48f384c27a2bd7141ada4f98d
Dita Annisaftia Rizky <32301025+Ditaannisaftiarizky@users.noreply.github.com>
2018-04-03 09:26:08 +0800

    membuat atribut dan behavior

--- src/praktek02/Square.java
+++ src/praktek02/Square.java
@@ -1,5 +1,15 @@
 
 package praktek02;
 public class Square {
+    double sisi1;
+    double sisi2;
+    
+  void cetakInfo () {
+      System.out.println("=================");
+      System.out.println("Sisi1 :"+sisi1);
+      System.out.println("Sisi2 :"+sisi2);
+      System.out.println("=================");
+       
+   }
     
 }
